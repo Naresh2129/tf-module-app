ba92bb312d5172efa30a9e6c1b2a563d6d2a9873
Naresh2129 <nareshboligorla@gmail.com>
2024-05-29 11:39:45 +0530

    tags

---
+++ vars.tf
@@ -0,0 +1,8 @@
+variable "env" {}
+variable "tags" {}
+variable "subnet_ids" {}
+variable "vpc_id" {}
+variable "component" {}
+variable "port" {}
+variable "sg_ingress_cidr" {}
+variable "ssh_ingress_cidr" {}
