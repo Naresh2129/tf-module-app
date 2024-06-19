6622ffaef04e9e3facc85db2c5826b7a9c1927ba
Naresh2129 <nareshboligorla@gmail.com>
2024-06-19 12:30:19 +0530

    tags

--- main.tf
+++ main.tf
@@ -93,11 +93,12 @@ resource "aws_lb_listener_rule" "main" {
 }
 
 resource "aws_lb_target_group" "public" {
-        count    = var.component == "frontend" ? 1 : 0
-        name     = "${local.name_prefix}-public"
-        port     = var.port
-        protocal = "HTTP"
-        vpc_id   = var.vpc_id
+        count       = var.component == "frontend" ? 1 : 0
+        name        = "${local.name_prefix}-public"
+        port        = var.port
+        target_type = "ip"
+        protocal    = "HTTP"
+        vpc_id      = var.default_vpc_id
 }
 
 resource "aws_lb_target_group_attachment" "public" {
@@ -105,4 +106,5 @@ resource "aws_lb_target_group_attachment" "public" {
         target_group_arn = aws_lb_target_group.public[0].arn
         target_id        =  element(data.dns_a_record_set.private_alb.addrs, count.index)
         port             = 80
+        availability_zone = all
 }
